1e14fda5d5d0c7465066c84aa9dcec3bcc600ce0
hyedol2 <126440965+hyedol2@users.noreply.github.com>
2026-07-26 01:29:19 +0900

    Update import statement for pointer input

--- app/src/main/java/com/example/customkeyboard/ui/RunScreen.kt
+++ app/src/main/java/com/example/customkeyboard/ui/RunScreen.kt
@@ -16,7 +16,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.pointerInput
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.style.TextAlign
